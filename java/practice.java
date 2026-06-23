826a60a4351f832d941558d8bd7a107fa3ba4204
Noirelle0004 <nyclutch11@gmail.com>
2026-06-23 15:04:46 +0800

    javapractice1

---
+++ java/practice.java
@@ -0,0 +1,9 @@
+public class practice{
+    public static void main(String[] args){
+        String name="Baobao";
+        int age = 36;
+        System.out.println("Hello,Java");
+        System.out.println("My name is"+name);
+        System.out.println("I'm "+age+" year old.");
+    }
+}
