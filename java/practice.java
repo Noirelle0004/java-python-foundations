daea61e431e1f33fddf78d77208e8b44250a3d53
Noirelle0004 <nyclutch11@gmail.com>
2026-06-23 15:23:19 +0800

    update

--- java/practice.java
+++ java/practice.java
@@ -1,9 +1,8 @@
 public class practice{
     public static void main(String[] args){
-        String name="Baobao";
-        int age = 36;
-        System.out.println("Hello,Java");
-        System.out.println("My name is"+name);
-        System.out.println("I'm "+age+" year old.");
+          String name="baobao";
+          int age= 36;
+          System.out.println("My name is "+name);
+          System.out.println("I'm "+age+" years old");
     }
 }
